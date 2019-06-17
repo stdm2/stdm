4460fb30e31a152747bb7c4e992572a8cfa01700
Kevin Ng'eno <kngeno.kevin@gmail.com>
2019-06-17 11:58:35 +0300

    Update deploy_tags.sh

--- scripts/deploy_tags.sh
+++ scripts/deploy_tags.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-zip -9 -r stdm-${TRAVIS_TAG}.zip stdm
-curl --ftp-create-dirs -T stdm-*.zip -u $FTP_USER:$FTP_PASSWORD ftp://$FTP_ADDRESS/tags/
+zip -9 -r stdm_${TRAVIS_TAG}.zip stdm
+curl --ftp-create-dirs -T stdm_*.zip -u $FTP_USER:$FTP_PASSWORD ftp://$FTP_ADDRESS/plugin/dev/
