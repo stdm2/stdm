33f6213925ff9ad63f10a1284a151ba33e9a5247
Kevin Ng'eno <kngeno.kevin@gmail.com>
2019-06-17 11:57:28 +0300

    Update deploy_master.sh

--- scripts/deploy_master.sh
+++ scripts/deploy_master.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-zip -9 -r  stdm-${TRAVIS_BRANCH}.zip stdm
-curl --ftp-create-dirs -T stdm-*.zip -u $FTP_USER:$FTP_PASSWORD ftp://$FTP_ADDRESS/master/
+zip -9 -r  stdm_${TRAVIS_BRANCH}.zip stdm
+curl --ftp-create-dirs -T stdm_*.zip -u $FTP_USER:$FTP_PASSWORD ftp://$FTP_ADDRESS/plugin/stable/
