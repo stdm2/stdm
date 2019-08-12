51cee70fd4fb0f8f8c83b20a0c5b669674937d1b
kngeno <kngeno.kevin@gmail.com>
2019-08-12 16:55:53 +0300

    coverage tests

---
+++ stdm/tests/test_numerals.py
@@ -0,0 +1,11 @@
+from numerals import sum, sum_only_positive
+
+def test_sum():
+    assert sum(5, 5) == 10
+
+def test_sum_positive_ok():
+    assert sum_only_positive(2, 2) == 4
+
+def test_sum_positive_fail():
+    assert sum_only_positive(-1, 2) is None
+
