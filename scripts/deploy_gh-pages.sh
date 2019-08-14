ea53a69fbd1ae0f1d04f49cb74b3b558d64bec07
kngeno <kngeno.kevin@gmail.com>
2019-08-14 19:48:50 +0300

    coveragerc edits

--- scripts/deploy_gh-pages.sh
+++ scripts/deploy_gh-pages.sh
@@ -1,23 +1,36 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# Push HTML files to gh-pages automatically.
 
-# build the docs
-cd docs
-make clean
-make html
+# Fill this out with the correct org/repo
+ORG=kngeno
+REPO=stdm2
+# This probably should match an email for one of your users.
+EMAIL=kngeno.kevin@gmail.com
+
+set -e
+
+# Clone the gh-pages branch outside of the repo and cd into it.
 cd ..
-# commit and push
-git add -A
-git commit -m "building and pushing docs"
-git push origin master
-# switch branches and pull the data we want
-git checkout gh-pages
-rm -rf .
-touch .nojekyll
-git checkout master docs/build/html
-mv ./docs/build/html/* ./
-rm -rf ./docs
-git add -A
-git commit -m "publishing updated docs..."
-git push origin gh-pages
-# switch back
-git checkout master
+git clone -b gh-pages "https://$STDM_TOKEN@github.com/$ORG/$REPO.git" gh-pages
+cd gh-pages
+
+# Update git configuration so I can push.
+if [ "$1" != "dry" ]; then
+    # Update git config.
+    git config user.name "$ORG"
+    git config user.email "$EMAIL"
+fi
+
+# Copy in the HTML.  You may want to change this with your documentation path.
+cp -R ../$REPO/docs/build/html/* ./
+rm -rf docs/ images/ scripts/ stdm/
+rm .coveragerc .coveralls.yml .readthedocs.yml .gitignore *.yml *.txt *.inv *.sh *.md CONTRIBUTORS.rst || :
+echo "clean github pages"
+
+# Add and commit changes.
+git add -A .
+git commit -m "[ci skip] Autodoc commit for $COMMIT."
+if [ "$1" != "dry" ]; then
+    # -q is very important, otherwise you leak your GH_TOKEN
+    git push -q origin gh-pages
+fi
