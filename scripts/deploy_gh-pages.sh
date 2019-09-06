ffe3fca61ca944e1283c531688df5a5131367e0e
kngeno <kngeno.kevin@gmail.com>
2019-09-06 16:35:24 +0300

    modified:   scripts/deploy_gh-pages.sh

--- scripts/deploy_gh-pages.sh
+++ scripts/deploy_gh-pages.sh
@@ -10,25 +10,35 @@ EMAIL=kngeno.kevin@gmail.com
 
 set -e
 
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_website_files() {
+  cd ..
+  echo "Cloning master branch"
+  git clone "https://$STDM_TOKEN@github.com/$ORG/$REPO.git" gh-pages > /dev/null
+  cd gh-pages
+  git push origin --delete gh-pages
+  git checkout -b gh-pages
+
+}
+
+upload_files() {
+  git remote add origin-pages https://$STDM_TOKEN@github.com/$ORG/$REPO.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin-pages gh-pages 
+}
+
+setup_git
+commit_website_files
+upload_files
+
 # Clone the gh-pages branch outside of the repo and cd into it.
-cd ..
-echo "Cloning master branch"
-git clone "https://$STDM_TOKEN@github.com/$ORG/$REPO.git" gh-pages > /dev/null
-cd gh-pages
-git push origin --delete https://$STDM_TOKEN@github.com/$ORG/$GH_PAGES.git
-# git remote add origin https://$STDM_TOKEN@github.com/$ORG/$REPO.git
-git push origin gh-pages > /dev/null
-git push --set-upstream origin gh-pages > /dev/null
+
 
 echo "Allow files with underscore https://help.github.com/articles/files-that-start-with-an-underscore-are-missing/" > .nojekyll
 echo "[View live](https://kngeno.github.io/stdm2/)"
-
-# Update git configuration so I can push.
-if [ "$1" != "dry" ]; then
-    # Update git config.
-    git config user.name "$ORG"
-    git config user.email "$EMAIL"
-fi
 
 # Copy in the HTML.  You may want to change this with your documentation path.
 echo "project path..."
