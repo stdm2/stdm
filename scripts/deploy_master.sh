3e01ecd2e524f894c8a05e769ab1f312768faec8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-10 18:20:39 +0000

    Deploy kngeno/stdm2 to github.com/kngeno/stdm2.git:gh-pages

--- scripts/deploy_master.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-zip -9 -r  stdm_${TRAVIS_BRANCH}.zip stdm
-curl --ftp-create-dirs -T stdm_*.zip -u $FTP_USER:$FTP_PASSWORD ftp://$FTP_ADDRESS/plugin/stable/
--- scripts/deploy_tags.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-zip -9 -r stdm_${TRAVIS_TAG}.zip stdm
-curl --ftp-create-dirs -T stdm_*.zip -u $FTP_USER:$FTP_PASSWORD ftp://$FTP_ADDRESS/plugin/dev/
