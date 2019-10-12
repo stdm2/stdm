fe1b5367a2d711651434a3e387b2f2b73aba8ea8
kngeno <kngeno.kevin@gmail.com>
2019-10-12 23:24:55 +0300

    update api autodoc

--- stdm/gui/module.py
+++ stdm/gui/module.py
@@ -1,21 +1,25 @@
 """
 /***************************************************************************
+
 Name                 : STDM Module
 Description          : Abstract container that contains information for
+                       
                        specific functionality.
+
 Date                 : 26-05-2019
 copyright            : (C) 2019 by UN-Habitat and implementing partners.
-                       See the accompanying file CONTRIBUTORS.txt in the root
+contributors         : See the accompanying file CONTRIBUTORS.txt in the root
 email                : stdm@unhabitat.org
+
  ***************************************************************************/
 
 /***************************************************************************
- *                                                                         *
+
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
  *   the Free Software Foundation; either version 3 of the License, or     *
  *   (at your option) any later version.                                   *
- *                                                                         *
+ 
  ***************************************************************************/
 """
 
@@ -24,8 +28,8 @@ from abc import (
     abstractmethod
 )
 from PyQt5.QtGui import QIcon
-from PyQt.QtWidgets import QAction
-from qgis.gui import QgisInterface
+# from PyQt.QtWidgets import QAction
+# from qgis.gui import QgisInterface
 from stdm.core.security import PermissionInfo
 
 
--- stdm/tests/gui/test_stdm_module.py
+++ stdm/tests/gui/test_stdm_module.py
@@ -4,22 +4,23 @@ Name                 : StdmModule Unit Tests
 Description          : Unit tests for StdmModule class.
 Date                 : 26-05-2019
 copyright            : (C) 2019 by UN-Habitat and implementing partners.
-                       See the accompanying file CONTRIBUTORS.txt in the root
+contributors         : See the accompanying file CONTRIBUTORS.txt in the root
 email                : stdm@unhabitat.org
- ***************************************************************************/
+
+***************************************************************************/
 
 /***************************************************************************
- *                                                                         *
+
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
  *   the Free Software Foundation; either version 3 of the License, or     *
  *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
+
+***************************************************************************/
 """
 
 import unittest
-from qgis.utils import iface
+# from qgis.utils import iface
 from stdm.gui.module import StdmModule
 from stdm.tests.gui.custom_modules import (
     ConfigurationModule,
