97c69e3385554fe8a19c1e3deadb206c52eb6331
kngeno <kngeno.kevin@gmail.com>
2019-10-14 16:02:37 +0300

    catch pycodestyle errors

--- stdm/plugin.py
+++ stdm/plugin.py
@@ -29,15 +29,16 @@ import subprocess
 
 # Import the PyQt and QGIS libraries
 from PyQt5.QtCore import QFileInfo, QUrl, QFile, QDir, QSettings
-from PyQt5.QtWidgets import (
-    QAction, QFileDialog, QMessageBox)
+from PyQt5.QtWidgets import QAction, QFileDialog, QMessageBox
 
-from PyQt5.QtGui import (QIcon,
-                         QDesktopServices,
-                         QStandardItemModel,
-                         QStandardItem
-                         )
+from PyQt5.QtGui import (
+    QIcon,
+    QDesktopServices,
+    QStandardItemModel,
+    QStandardItem,
+)
 from qgis.core import QgsApplication
+
 # Initialize Qt resources from file resources.py
 # Do not remove this import even though your IDE / pylint may report it unused
 # noinspection PyUnresolvedReferences
@@ -57,9 +58,10 @@ class STDMQGISLoader:
         # Save reference to the QGIS interface
         self.iface = iface
         # noinspection PyArgumentList
-        self.user_plugin_dir = QFileInfo(
-            QgsApplication.qgisUserDatabaseFilePath()).path() + \
-            '/python/plugins'
+        self.user_plugin_dir = (
+            QFileInfo(QgsApplication.qgisUserDatabaseFilePath()).path()
+            + "/python/plugins"
+        )
         self.plugin_builder_path = os.path.dirname(__file__)
 
         # class members
@@ -75,19 +77,21 @@ class STDMQGISLoader:
         """Create the menu entries and toolbar icons inside the QGIS GUI."""
         # Create action that will start plugin configuration
         self.action = QAction(
-            QIcon(':/plugins/stdm/icon.png'),
-            'Placeholder', self.iface.mainWindow())
+            QIcon(":/plugins/stdm/icon.png"),
+            "Placeholder",
+            self.iface.mainWindow(),
+        )
         # connect the action to the run method
         self.action.triggered.connect(self.run)
 
         # Add toolbar button and menu item
         self.iface.addToolBarIcon(self.action)
-        self.iface.addPluginToMenu('&STDM', self.action)
+        self.iface.addPluginToMenu("&STDM", self.action)
 
     def run(self):
         pass
 
     def unload(self):
         """Removes the plugin menu item and icon from QGIS GUI."""
-        self.iface.removePluginMenu('&STDM', self.action)
+        self.iface.removePluginMenu("&STDM", self.action)
         self.iface.removeToolBarIcon(self.action)
