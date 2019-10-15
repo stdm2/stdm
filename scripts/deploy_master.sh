fe9f502c37986b594b1b57dfaa7c33538946814d
kngeno <kngeno.kevin@gmail.com>
2019-10-15 09:49:40 +0300

    ftp push

--- scripts/deploy_master.sh
+++ scripts/deploy_master.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 zip -9 -r  stdm_${TRAVIS_BRANCH}.zip stdm
-curl --ftp-create-dirs -T stdm_*.zip -u -v $FTP_USER:$FTP_PASSWORD ftp://$FTP_ADDRESS:$FTP_PORT/plugin/stable/
+curl --ftp-create-dirs  --output --fail -P $FTP_PORT -T stdm_*.zip -u $FTP_USER:$FTP_PASSWORD ftp://$FTP_ADDRESS/plugin/stable/
--- scripts/deploy_tags.sh
+++ scripts/deploy_tags.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 zip -9 -r stdm_${TRAVIS_TAG}.zip stdm
-curl --ftp-create-dirs -T stdm_*.zip -u -v $FTP_USER:$FTP_PASSWORD ftp://$FTP_ADDRESS:$FTP_PORT/plugin/dev/
+curl --ftp-create-dirs -T stdm_*.zip -u $FTP_USER:$FTP_PASSWORD ftp://$FTP_ADDRESS:$FTP_PORT/plugin/dev/
